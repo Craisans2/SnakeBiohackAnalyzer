6bef887ea43a88335a7c2ffe8b2ade707d3133cc
JuppyGuppy <dylangotrent@gmail.com>
2023-11-04 13:29:17 -0400

    morre to imaage parser

--- vidAnalysis.py
+++ vidAnalysis.py
@@ -1,6 +1,19 @@
-import cv2
+import cv2 as cv
 #hello world
 #imports
 #Function to seperate the video into images
 def imageParser(video_file):
     vidMedCap = cv2.VideoCapture(video_file)
+    frameNum = 0
+    frames = [] 
+    while(True):
+        #process the frames
+        
+        success,frame = vidMedCap.read()
+        if success:
+            cv2.imwrite(frames,frame)
+        else:
+            break
+        
+        frameNum=frameNum+1
+vidMedCap.release()
