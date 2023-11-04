102b2254bfc04600ddf13496679497cf87d1408c
David Butz <david.butz26@gmail.com>
2023-11-04 13:52:24 -0400

    Pip installs

--- vidAnalysis.py
+++ vidAnalysis.py
@@ -1,4 +1,9 @@
 import cv2 as cv
+pip install tensorflow
+pip install tf_slim
+pip install tf-models-official
+pip install opencv-python
+
 #hello world
 #imports
 #Function to seperate the video into images
