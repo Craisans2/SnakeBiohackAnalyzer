53779af6e599fe9e9a51092b4607b24cec3d8f14
JuppyGuppy <dylangotrent@gmail.com>
2023-11-04 14:00:44 -0400

    Commiting now beacue stoopid

--- vidAnalysis.py
+++ vidAnalysis.py
@@ -1,19 +1,20 @@
-import cv2 as cv
+import cv as cv2
 #hello world
 #imports
+#dylan  testing if his stupid code works
+imageParser('C:/Users/dylan/OneDrive/Desktop/Biohack/IMG_4688.MOV')
 #Function to seperate the video into images
 def imageParser(video_file):
     vidMedCap = cv2.VideoCapture(video_file)
     frameNum = 0
-    frames = [] 
     while(True):
         #process the frames
         
         success,frame = vidMedCap.read()
         if success:
-            cv2.imwrite(frames,frame)
+            cv2.imwrite(f'C:/Users/dylan/OneDrive/Desktop/Biohack/Images',frame)
         else:
             break
         
         frameNum=frameNum+1
-vidMedCap.release()
+    vidMedCap.release()
