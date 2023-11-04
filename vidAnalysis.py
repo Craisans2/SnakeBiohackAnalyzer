e5736772079afdac12a9c50df8db8f5f41aac076
JuppyGuppy <dylangotrent@gmail.com>
2023-11-04 16:39:02 -0400

    my insane ramblings

--- vidAnalysis.py
+++ vidAnalysis.py
@@ -4,55 +4,53 @@ import tensorflow as tf
 import torch
 from transformers import DetrImageProcessor, DetrForObjectDetection
 
-# Load the pre-trained DETR model and processor
-model_name = "facebook/detr-resnet-50"
-processor = DetrImageProcessor.from_pretrained(model_name)
-model = DetrForObjectDetection.from_pretrained(model_name)
+#sectionImages
+def processIm(imPath):
+    # Load the pre-trained DETR model and processor
+    model_name = "facebook/detr-resnet-50"
+    processor = DetrImageProcessor.from_pretrained(model_name)
+    model = DetrForObjectDetection.from_pretrained(model_name)
 
-# Path to the image you want to analyze
-image_path = 'Users/davidbutz/Desktop/IMG_0018.jpeg'
+    # Path to the image you want to analyze
+    image_path = imPath
 
-# Load and preprocess the image
-image = processor(images=image_path, return_tensors="pt")
-outputs = model(**image)
+    # Load and preprocess the image
+    image = processor(images=image_path, return_tensors="pt")
+    outputs = model(**image)
 
-# Get the predicted boxes and labels
-pred_boxes = outputs.logits["pred_boxes"]
-pred_logits = outputs.logits["pred_logits"]
+    # Get the predicted boxes and labels
+    pred_boxes = outputs.logits["pred_boxes"]
+    pred_logits = outputs.logits["pred_logits"]
 
-# Threshold for detection confidence
-detection_threshold = 0.5
+    # Threshold for detection confidence
+    detection_threshold = 0.5
 
-# Filter detections based on confidence score
-detections = pred_boxes[0][pred_logits[0][:, 0] > detection_threshold]
+    # Filter detections based on confidence score
+    detections = pred_boxes[0][pred_logits[0][:, 0] > detection_threshold]
 
-# You can now work with the detected bounding boxes (detections)
-# You may want to visualize or process the results further as needed
+    # You can now work with the detected bounding boxes (detections)
+    # You may want to visualize or process the results further as needed
 
-# Example: Print the coordinates of detected scalpel boxes
-for box in detections:
-    print("Box Coordinates:", box.tolist())
+    # Example: Print the coordinates of detected scalpel boxes
+    for box in detections:
+        print("Box Coordinates:", box.tolist())
 
-# Example: Visualize the detected boxes (you can use a library like matplotlib)
-import matplotlib.pyplot as plt
-import matplotlib.patches as patches
-from PIL import Image
+    # Example: Visualize the detected boxes (you can use a library like matplotlib)
+    import matplotlib.pyplot as plt
+    import matplotlib.patches as patches
+    from PIL import Image
 
-image = Image.open(image_path)
-plt.imshow(image)
+    image = Image.open(image_path)
+    plt.imshow(image)
 
-ax = plt.gca()
-for box in detections:
-    xmin, ymin, xmax, ymax = box.tolist()
-    rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, linewidth=1, edgecolor="r", facecolor="none")
-    ax.add_patch(rect)
+    ax = plt.gca()
+    for box in detections:
+        xmin, ymin, xmax, ymax = box.tolist()
+        rect = patches.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, linewidth=1, edgecolor="r", facecolor="none")
+        ax.add_patch(rect)
 
-plt.show()
+    plt.show()
 
-
-
-#hello world
-#imports
 #Function to seperate the video into images
 def imageParser(video_file,imageFile):
     vidMedCap = cv.VideoCapture(video_file)
@@ -68,11 +66,7 @@ def imageParser(video_file,imageFile):
         
         frameNum=frameNum+1
     vidMedCap.release()
-    
-
-def imageAnalyzer():
-    #here
-
+    return frameNum  
 
 #Presurgery Risk factors that increase infection risk
 highrisk = False
@@ -98,4 +92,10 @@ hasSwell = bool(input("does the patient have excess swelling around the incision
 
 if hasSepsis or hasInfectionHistory or hasSwell:
     highrisk = True
-    
+
+videoFile = 'C:/Users/dylan/OneDrive/Desktop/Biohack/IMG_4688.MOV'
+imageFile = 'C:/Users/dylan/OneDrive/Desktop/Biohack/Images'
+frameNum = imageParser(videoFile,imageFile)
+
+for i in range(frameNum):
+    processIm('{imageFile}/frame_{frameNum}.jpg')
