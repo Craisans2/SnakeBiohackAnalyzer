904de1bb85f6516f410015d41e5e6279205185ed
JuppyGuppy <dylangotrent@gmail.com>
2023-11-04 14:16:32 -0400

    Dylan made his method work :D

--- vidAnalysis.py
+++ vidAnalysis.py
@@ -16,7 +16,7 @@ def imageParser(video_file):
         
         success,frame = vidMedCap.read()
         if success:
-            cv.imwrite(f'C:/Users/dylan/OneDrive/Desktop/Biohack/Images',frame)
+            cv.imwrite(f'C:/Users/dylan/OneDrive/Desktop/Biohack/Images/frame_{frameNum}.jpg',frame)
         else:
             break
         
@@ -34,12 +34,14 @@ hasObesity = False
 hasSmokes = False
 
 #Inputs for Risks
-hasDiabetes = boolean(input("does the patient have diabetes: enter 1 or 0: "))
-hasInfectionHistory = boolean(input("does the patient have prior infection history: enter 1 or 0: "))
-hasObesity = boolean(input("does the patient have obesity: enter 1 or 0: "))
-hasSmokes = boolean(input("does the patient smoke: enter 1 or 0: "))
-hasCancer = boolean(input("does the patient have cancer or is in remission: enter 1 or 0: "))
+hasDiabetes = bool(input("does the patient have diabetes: enter 1 or 0: "))
+hasInfectionHistory = bool(input("does the patient have prior infection history: enter 1 or 0: "))
+hasObesity = bool(input("does the patient have obesity: enter 1 or 0: "))
+hasSmokes = bool(input("does the patient smoke: enter 1 or 0: "))
+hasCancer = bool(input("does the patient have cancer or is in remission: enter 1 or 0: "))
 
 if hasDiabetes or hasInfectionHistory or hasObesity or hasDiabetes or hasSmokes or hasCancer:
     highrisk = True
 
+videoCam = str(input("please enter the video you wish to analyze"))
+imageParser('C:/Users/dylan/OneDrive/Desktop/Biohack/IMG_4688.MOV')
