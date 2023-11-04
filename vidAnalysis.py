7603c4eb2f5634b785b418be4843b3477382ebe4
JuppyGuppy <dylangotrent@gmail.com>
2023-11-04 14:01:17 -0400

    This is not an empty ocmming message

--- vidAnalysis.py
+++ vidAnalysis.py
@@ -1,20 +1,21 @@
-import cv as cv2
+import cv2 as cv
 #hello world
 #imports
 #dylan  testing if his stupid code works
-imageParser('C:/Users/dylan/OneDrive/Desktop/Biohack/IMG_4688.MOV')
 #Function to seperate the video into images
 def imageParser(video_file):
-    vidMedCap = cv2.VideoCapture(video_file)
+    vidMedCap = cv.VideoCapture(video_file)
     frameNum = 0
     while(True):
         #process the frames
         
         success,frame = vidMedCap.read()
         if success:
-            cv2.imwrite(f'C:/Users/dylan/OneDrive/Desktop/Biohack/Images',frame)
+            cv.imwrite(f'C:/Users/dylan/OneDrive/Desktop/Biohack/Images',frame)
         else:
             break
         
         frameNum=frameNum+1
     vidMedCap.release()
+
+imageParser('C:/Users/dylan/OneDrive/Desktop/Biohack/IMG_4688.MOV')
