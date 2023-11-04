e04811ff32f823d9baaa542876be9fe93af388b2
SkyTrent <skylarbotrent@gmail.com>
2023-11-04 14:04:44 -0400

    bools

--- vidAnalysis.py
+++ vidAnalysis.py
@@ -7,7 +7,8 @@ pip install opencv-python
 #hello world
 #imports
 #Function to seperate the video into images
-def imageParser(video_file):
+#def imageParser(video_file):
+
     vidMedCap = cv2.VideoCapture(video_file)
     frameNum = 0
     frames = [] 
@@ -28,18 +29,17 @@ vidMedCap.release()
 #Presurgery Risk factors that increase infection risk
 highrisk = False
 
-hasDiabetes = False
-hasInfectionHistory = False
-hasObesity = False
-hasSmokes = False
 
 #Inputs for Risks
-hasDiabetes = boolean(input("does the patient have diabetes: enter 1 or 0: "))
-hasInfectionHistory = boolean(input("does the patient have prior infection history: enter 1 or 0: "))
-hasObesity = boolean(input("does the patient have obesity: enter 1 or 0: "))
-hasSmokes = boolean(input("does the patient smoke: enter 1 or 0: "))
-hasCancer = boolean(input("does the patient have cancer or is in remission: enter 1 or 0: "))
+hasDiabetes = bool(input("does the patient have diabetes: enter 1 or 0: "))
+hasInfectionHistory = bool(input("does the patient have prior infection history: enter 1 or 0: "))
+hasObesity = bool(input("does the patient have obesity: enter 1 or 0: "))
+hasSmokes = bool(input("does the patient smoke: enter 1 or 0: "))
+hasCancer = bool
+(input("does the patient have cancer or is in remission: enter 1 or 0: "))
 
 if hasDiabetes or hasInfectionHistory or hasObesity or hasDiabetes or hasSmokes or hasCancer:
     highrisk = True
+    print("the patient is high risk")
+
 
